153f341f52c398e6d4413fe25125799667f6cf66
YB009 <owohdaniel09@gmail.com>
2025-08-11 19:31:21 +0100

    NEW COMMIT

--- routes/index.js
+++ routes/index.js
@@ -19,7 +19,7 @@ const site = {
     lat: 54.989337,
     lng: -1.44432,
     embedSrc:
-      "https://www.google.com/maps?q=54.989337,-1.444320&z=15&output=embed",
+      "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2289.1234567890123!2d-1.44432!3d54.989337!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x0!2zNTTCsDU5JzIxLjYiTiAxwrAyNiczOS42Ilc!5e0!3m2!1sen!2suk!4v1234567890123",
   },
   usp: [
     "Mobile service, we come to you",
